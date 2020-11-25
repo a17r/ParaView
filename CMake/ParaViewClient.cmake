7fe2ebc709ecb3e7f3b2aa6fc124dda0622541d4
Andreas Sturmlechner <asturm@gentoo.org>
2020-11-25 16:32:56 +0100

    Make qt_xmlpatterns_executable overridable
    So far this relies on fragile guessing of path or binary being in PATH.
    
    Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

--- CMake/ParaViewClient.cmake
+++ CMake/ParaViewClient.cmake
@@ -512,11 +512,13 @@ function (paraview_client_documentation)
   endif ()
 
   include("${_ParaViewClient_cmake_dir}/paraview-find-package-helpers.cmake" OPTIONAL)
-  find_program(qt_xmlpatterns_executable
-    NAMES xmlpatterns-qt5 xmlpatterns
-    HINTS "${Qt5_DIR}/../../../bin"
-          "${Qt5_DIR}/../../../libexec/qt5/bin"
-    DOC   "Path to xmlpatterns")
+  if (NOT qt_xmlpatterns_executable)
+    find_program(qt_xmlpatterns_executable
+      NAMES xmlpatterns-qt5 xmlpatterns
+      HINTS "${Qt5_DIR}/../../../bin"
+            "${Qt5_DIR}/../../../libexec/qt5/bin"
+      DOC   "Path to xmlpatterns")
+  endif ()
   mark_as_advanced(qt_xmlpatterns_executable)
 
   if (NOT qt_xmlpatterns_executable)
